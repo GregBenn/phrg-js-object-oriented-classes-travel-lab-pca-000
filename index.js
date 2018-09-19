43fdfce2412dba6db801139bb2ba2fea5398380d
Greg Benn <gbenn07@gmail.com>
2018-09-19 17:00:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  };
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.startDate.getFullYear();
+  };
+}
+
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  };
+
+  avenueInt(avenue) {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    return eastWest.indexOf(avenue);
+  }
+
+  blocksTravelled() {
+    let horizontal = this.avenueInt(this.endingLocation.horizontal) - this.avenueInt(this.beginningLocation.horizontal);
+    let vertical = this.endingLocation.vertical - this.beginningLocation.vertical;
+    return horizontal + vertical;
+  }
+
+  estimatedTime(peek) {
+    if (peek) {
+      return this.blocksTravelled()/2
+    } else {
+      return this.blocksTravelled()/3
+    }
+  }
+}
